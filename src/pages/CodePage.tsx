1576e58e3f818ee9d8f36dd4d86658926cbe5dc7
9348abinash-nayak <nayakabiansh746@gmail.com>
2026-04-22 15:26:54 +0530

    fix invite url

--- src/pages/CodePage.tsx
+++ src/pages/CodePage.tsx
@@ -467,8 +467,8 @@ const handleMovecoursor = (pos) => {
     if (navigator.share) {
       await navigator.share({
         title: "Join Room",
-        text: message,
-        url: link,
+        // text: message,
+       text: `Join my coding room: ${link}`,
       });
     } else {
       await navigator.clipboard.writeText(link);
